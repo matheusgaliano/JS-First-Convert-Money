cee26ac1d229ae038864a8b8740d5a83c06b40f7
matheusgaliano <matheus.galiano.silva@gmail.com>
2025-07-11 14:44:30 -0300

    zip

---
+++ OperadoresAritmeticos/operadores-aritmeticos.js
@@ -0,0 +1,20 @@
+/* 
+
+
+Operadores Aritméticos
+
++ Adição
+- Subtração
+/ Divisão
+* Multiplicação 
+% Resto
+++ Incremento
+-- Decremento
+** Exponencial
+
+*/
+
+
+const result = 2+3
+
+console.log(result)
