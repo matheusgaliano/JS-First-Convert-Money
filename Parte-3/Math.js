10c7101dbb3f311fd5bc6831c286c463b311c0c7
matheusgaliano <matheus.galiano.silva@gmail.com>
2025-07-11 11:42:44 -0300

    Projeto Random

---
+++ Parte-3/Math.js
@@ -0,0 +1,15 @@
+/* 
+Math -> Matemática
+    - pow(2,2) / potência / 2 elevado ao quadrado = 4 / power
+    - sqrt(25,2) / raiz de 25
+    - PI 3,14
+    - ceil / teto -> arredondar pra cima
+    - floor / chão - arredondar pra baixo
+
+    - random() / numero aleatório entre 0 e 1
+*/
+
+
+
+const result = Math.PI
+console.log(result)
